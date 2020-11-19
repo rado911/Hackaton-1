a0833e986ab61017e7834cfc077d32cfebebac64
MattWilmot <mattwilmot98@gmail.com>
2020-11-19 14:46:41 +0100

    started entry page

---
+++ src/components/entry-page.js
@@ -0,0 +1,18 @@
+import Axios from 'axios'
+import React, { useState } from 'react';
+
+
+const EntryPage = () => {
+   
+    return(
+        <>
+        <h1>Buy Art</h1>
+        
+        <h1>Sell Art</h1>
+
+        </>
+    )
+}
+
+
+export default EntryPage
