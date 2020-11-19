e3a72eafa37e9f72fcbf3edc154ab6a6b1ee1823
MattWilmot <mattwilmot98@gmail.com>
2020-11-19 17:20:36 +0100

    added background image to entry page

--- src/components/entry-page.js
+++ src/components/entry-page.js
@@ -1,43 +1,24 @@
 import React, { useState, useContext, useEffect } from 'react';
 import ArtContext from '../context/ArtContext';
+import './entry-page.css'
 
 
 const EntryPage = () => {
 
     const {art} = useContext(ArtContext); 
-    const [loader, setLoader] = useState(true)
-
-    const [background, setBackground] = useState([])
-
-    const backgroundArray = []
-    const backgroundPush = () => {
-        {art.slice(50).map(e=> backgroundArray.push(`${e.webImage.url}`))}
-
-    }
-
-useEffect(()=>{
-
-    backgroundPush()
-    // function randomInteger(min, max) {
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    //   }
-    console.log(backgroundArray)
-
-
-
-    
-}, [loader])
-
-
-
     return(
+        <>
+        <div className="textCont">
+        <h1 className="title">Buy Art</h1>
         
-        <div className="backgroundImage" style={{backgroundImage:`url(https://lh3.googleusercontent.com/aKEZzQMkmdQ_j3TAieahX0YNq6IdF180754RUN_8e8frbxMFJnHXw5lBW49lASh7khTc_5hRSLVBGylMm-fazdO2UR0=s0)`}}>
-        <h1>Buy Art</h1>
-        
-        <h1>Sell Art</h1>
+        <h1 className="title">Sell Art</h1>
         </div>
-        
+
+        <div className="backgroundInfo">
+            <p>Nachtwacht, Schutters van wijk II onder leiding van kapitein Frans Banninck Cocq</p>
+            <p>Rembrandt van Rijn</p>
+        </div>
+        </>
     )
 }
 
